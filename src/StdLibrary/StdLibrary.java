c5be92d1dba0a97536bd3d26be0b7db22cfbd405
Dustin McDonnell <89492902+Dustin-McDonnell@users.noreply.github.com>
2024-03-08 05:19:28 -0500

    comments

--- src/StdLibrary/StdLibrary.java
+++ src/StdLibrary/StdLibrary.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class StdLibrary {
+    //Finds the mean
     public double findMean(double[] userInput){
         //2 options, using an array or an array list
         //find the sum
@@ -16,6 +17,7 @@ public class StdLibrary {
         return result;
     }
 
+    //Finds the Median
     public double findMedian(double[] userInput){
 
         ArrayList<Integer> test = new ArrayList<>();
@@ -42,6 +44,7 @@ public class StdLibrary {
         return median;
     }
 
+    //Finds the mode
     public Double findMode(double[] userInput){
         int highest = 1;
         Double mode = null;
@@ -65,6 +68,7 @@ public class StdLibrary {
             return mode;
         }
     }
+    //Finds Standard Deviation
     public Double standardDeviation(double[] userInput){
         double mean = findMean(userInput);
         double variance = 0;
@@ -79,6 +83,7 @@ public class StdLibrary {
         return variance;
     }
 
+    //Finds the factorial of a number using long
     public long factorial(long userInput){
         long result = 1;
         for (long i = userInput; i > 0; i --){
@@ -86,7 +91,7 @@ public class StdLibrary {
         }
         return result;
     }
-
+    //Finds the factorial of a number using double
     public double factorial(double userInput){
         double result = 1;
         for (double i = userInput; i > 0; i --){
@@ -95,6 +100,7 @@ public class StdLibrary {
         return result;
     }
 
+    //Finds the factorial of a number using big integer
     public BigInteger factorialBigInteger(long userInput){
         BigInteger result = BigInteger.valueOf(1);
         for (long i = userInput; i > 0; i--){
@@ -103,13 +109,14 @@ public class StdLibrary {
         return result;
 
     }
-    //Wire the pokemon game to another terminal
+    //Finds the permutation of N and R
     public Double permutation(double userInputN, double userInputR){
         double nFactorial = factorial(userInputN);
         double denominator = userInputN - userInputR;
         return nFactorial/factorial(denominator);
     }
 
+    //Finds the combinations
     public Double combinations(double userInputN, double userInputR){
         double nFactorial = factorial(userInputN);
         double denominator = userInputN - userInputR;
@@ -117,6 +124,7 @@ public class StdLibrary {
         return nFactorial/denominator;
         }
 
+        //Finds the binomial distribution
     public Double binomialDistribution(double trials, double successProb, double fail, double successNumber){
         double combination = combinations(trials, successNumber);
         successProb = Math.pow(successProb, successNumber);
@@ -125,11 +133,13 @@ public class StdLibrary {
         return answer;
     }
 
+    //Finds the conditional probability
     public Double conditionalProbability(double aIntersectB, double b){
         double conditionalProbability = aIntersectB/b;
         return conditionalProbability;
     }
 
+    //Determines independence between a conditional probability and an event
     public Boolean determineIndependence(double conditional, double firstEvent){
         if (conditional == firstEvent){
             return true;
@@ -137,6 +147,7 @@ public class StdLibrary {
         return false;
     }
 
+    //Determines independence between an intersection and events A and B
     public Boolean determineIndependence(double aIntersectB, double a, double b){
         if(aIntersectB == a*b){
             return true;
@@ -154,106 +165,64 @@ public class StdLibrary {
         return result;
     }
 
-    //https://www.tutorialspoint.com/java/java_generics.htm This is where I learned about generics
-    //This seems horribly inefficient
-    public <T, K> void union(ArrayList<T> set, ArrayList<K> subSet) {
-        //Remove Duplicates
-        for (int i = 0; i < subSet.size(); i++) {
-            for (int k = 0; k < set.size(); k++) {
-                //Why does .equals() work but not ==
-                if (subSet.get(i).equals(set.get(k))) {
-                    set.remove(k);
-                }
-            }
-        }
-        //Unions the sets
-        for (int j = 0; j < subSet.size(); j++) {
-            set.add((T) subSet.get(j));
-        }
-        System.out.println("Union of set and subset, not sorted: " + set);
-    }
-
-    public <T, K> ArrayList<T> complement(ArrayList<T> set, ArrayList<K> subSet) {
-        //Remove Duplicates
-        for (int i = 0; i < subSet.size(); i++) {
-            for (int k = 0; k < set.size(); k++) {
-                if (subSet.get(i).equals(set.get(k))) {
-                    set.remove(k);
-                }
-            }
-        }
-        System.out.println("Complement of subset: " + set);
-        return set;
-    }
-
-    public <T, K> void intersection(ArrayList<T> set, ArrayList<K> subSet) {
-        //Remove Duplicates
-        ArrayList<T> intersectSet = new ArrayList<T>();
-        for (int i = 0; i < subSet.size(); i++) {
-            for (int k = 0; k < set.size(); k++) {
-                if(subSet.get(i).equals(set.get(k))){
-                    intersectSet.add(set.get(k));
-                }
-            }
-        }
-        System.out.println("Intersection of set and subset: " + intersectSet);
-    }
-
+    //Finds the maen of binomial distribution
     public double expectedBinomialDistribution(double n, double p){
         double expected = n * p;
         return expected;
     }
-
+    //Finds the variance of binomial distribution
     public double varianceBinomialDistribution(double n, double p, double q){
         double variance = n * p * q;
         return variance;
     }
-
+    //Finds the geometric distribution
     public double geometricDistribution(double q, double y, double p){
         q = Math.pow(q, y-1);
         double result = q*p;
         return result;
     }
-
+    //Finds the mean of Geometric Distribution
     public double expectedGeometricDistribution(double p){
         double result = 1/p;
         return result;
     }
-
+    //Finds the variance of Geometric Distribution
     public double varianceGeometricDistribution(double p){
         double numerator = 1 - p;
         double denominator = Math.pow(p,2);
         double result = numerator/denominator;
         return result;
     }
-
+    //Finds the HyperGeometric Distribution
     public double hyperGeometricDistribution(double r, double y, double N, double n){
         double numerator = permutation(r,y) * permutation(N-r,n-y);
         double denominator = permutation(N,n);
         double result = numerator/denominator;
         return result;
     }
+    //Finds the mean of the HyperGeometricDistribution
     public double expectedHyperGeometricDistribution(double n, double r, double N){
         double result = (n*r)/N;
         return result;
     }
-
+    //Finds the variance of the HyperGeometric Distribution
     public double varianceHyperGeometricDistribution(double n, double r, double N){
         double result = n * (r/n) * ((N-r)/N) * ((N-n)/(N-1));
         return result;
     }
-
+    //Finds the NegativeBinomial Probability Distribution
     public double negativeBinomialProbabilityDistribution(double y, double r, double p, double q){
         Math.pow(p,r);
         double result = combinations(y-1,r-1) * Math.pow(p,r) * Math.pow(q,(y-r));
         return result;
     }
-
+    //Finds the mean of the Negative Binomial Probability Distribution
     public double expectedNegativeBinomialProbabilityDistribution(double r, double p){
         double result = r/p;
         return result;
     }
 
+    //Finds the variance of the Negative Binomial Probability Distributionb
     public double varianceNegativeBinomialProbabilityDistribution(double r, double p){
         double result = (r*(1-p))/Math.pow(p,2);
         return result;
